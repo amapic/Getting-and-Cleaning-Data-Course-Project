10903846cd63e6efd61fbc6deb52d485c35d8e70
amapic <amaury.pichat@gmail.com>
2014-12-21 22:16:25 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 #we add data set from train and test
-  X_test<-read.table(file="test/X_test.txt",header=F,sep="")
-  X_train<-read.table(file="train/X_train.txt",header=F,sep="")
+  X_test<-read.table(file="X_test.txt",header=F,sep="")
+  X_train<-read.table(file="X_train.txt",header=F,sep="")
   
   data<-rbind(X_train,X_test)
   
@@ -14,21 +14,22 @@
   
   filter3<-subset(features, grepl("std()", features[,2]) , drop = FALSE)
   datafilter<-rbind(filter2,filter3)
+  #V1 is the name of the column containing measurement number
   datafilter<-datafilter[order(datafilter$V1),]
   
   #we keep in the original data only the column containing mean() and std()
-  dataMeanStd<-data[,data2[,1]]
-  colnames(dataMeanStd)<-data2[,2]
+  dataMeanStd<-data[,datafilter[,1]]
+  colnames(dataMeanStd)<-datafilter[,2]
   
   #we add the subject data from train and test
-  subject_train<-read.table(file="train/subject_train.txt",header=F,sep="")
-  subject_test<-read.table(file="test/subject_test.txt",header=F,sep="")
+  subject_train<-read.table(file="subject_train.txt",header=F,sep="")
+  subject_test<-read.table(file="subject_test.txt",header=F,sep="")
   dataSubject<-rbind(subject_train,subject_test)
   colnames(dataSubject)<-c("Subject")
   
   #we add the activity data from train and test
-  y_train<-read.table(file="train/y_train.txt",header=F,sep="")
-  y_test<-read.table(file="test/y_test.txt",header=F,sep="")
+  y_train<-read.table(file="y_train.txt",header=F,sep="")
+  y_test<-read.table(file="y_test.txt",header=F,sep="")
   dataLabels<-rbind(y_train,y_test)
   colnames(dataLabels)<-c("Activity")
   
@@ -45,4 +46,3 @@
   #Point 6. we calculate the mean for each variable for each couple of subject and activity
   groupby <- datafinal %>% group_by(Subject,Activity)
   EachActivitySubjectMean<-groupby %>% summarise_each(funs(mean))
-  
